c60e16cad9a9855e2fb1d0817abe68c5a836482c
rajataghi <rajataghi@gmail.com>
2020-04-01 23:25:53 -0400

    Adding app

---
+++ CussCard/index.php
@@ -0,0 +1,25 @@
+
+<html>
+<body>
+<form action = "" method = "post" align = center>
+			<div class="form-group">
+			<label for="Word">Word</label>
+			<input type="text" class="form-control" id="fname" name="fname" placeholder="Enter first name">
+			</div>
+			<div class="form-group">
+			<label for="Pronunciation">Pronunciation</label>
+			<input type="text" class="form-control" id="lname" name="lname" placeholder="Enter last name">
+			</div>
+			<div class="form-group">
+			<label for="Meaning">Meaning</label>
+			<input type="text" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+			</div>
+		
+			
+			<button type="submit" name = "submitted" class="btn btn-primary">Submit</button>
+		</form>
+       
+      </div>
+    </div>
+</body>	
+</html>	
